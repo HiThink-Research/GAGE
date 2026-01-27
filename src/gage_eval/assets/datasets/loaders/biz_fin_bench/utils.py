dc68adefdeaecb508070cece95664ea57562c540
zhuwenqiao <zhuwenqiao@myhexin.com>
2026-01-27 11:11:20 +0800

    [bizfinbench] AEA_2

--- src/gage_eval/assets/datasets/loaders/biz_fin_bench/utils.py
+++ src/gage_eval/assets/datasets/loaders/biz_fin_bench/utils.py
@@ -2,6 +2,8 @@ def get_file_path(local_dir, split, subset):
     filename = ''
     if split == 'stock_price_predict' and subset == 'en':
         filename = 'stock_price_predict_en.jsonl'
+    elif split == 'anomaly_information_tracing' and subset == 'en':
+        filename = 'anomaly_information_tracing_en.jsonl'
 
     file_path = f'{local_dir}/{subset}/{filename}'
 
